0f78bb9bbcaf5218f7dac6286710f751628bb7aa
Joanna <joanna_pinczuk@hotmail.com>
2024-04-23 12:33:26 +0200

    updates

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
+
+btnStart.addEventListener('click', () => {
+  btnStop.removeAttribute('disabled');
+  btnStart.setAttribute('disabled', '');
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  s;
+});
+
+btnStop.addEventListener('click', ev => {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', '');
+  clearInterval(intervalId);
+});
