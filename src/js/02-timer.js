1429429fc305dbdcd6083eb5dfb902c9714f1c80
Joanna <joanna_pinczuk@hotmail.com>
2024-04-23 21:51:22 +0200

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,21 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const btnStart = document.querySelector('button[data-start]');
+const daysField = document.querySelector('span[data-days]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+    if (selectedDate.getTime() <= now.getTime()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      btnStart.removeAttribute('disabled');
+    }
+  },
+};
